3ab29fb96d6922eae20f7f080e1397e6f11c66ae
Prasanna48 <125577633+Prasanna48@users.noreply.github.com>
2023-02-21 19:36:19 +0530

    Add files via upload

---
+++ reverse of a number.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,r=0,remainder;
+	printf("enter an integer");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		remainder=n%10;
+		r=r*10+remainder;
+		n/=10;
+	}
+	printf("reversed number=%d",r);
+	return 0;
+}
